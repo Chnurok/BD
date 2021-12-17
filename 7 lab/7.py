72df84d61606d29c136c73c10673f4b8bf262a3b
Mikhail Smusev <39839108+Chnurok@users.noreply.github.com>
2021-12-17 18:49:41 +0300

    Update 7.py

--- 7 lab/7.py
+++ 7 lab/7.py
@@ -1,15 +1,15 @@
-import tarantool
 import pandas as pd
-from clickhouse_driver import Client
-import pandahouse as ph
-
+import tarantool
 connection = tarantool.connect("localhost", 3301, user='admin', password='pass')
 
 tester = connection.space('queue')
 
 
 data = tester.select()
+
 df = pd.DataFrame(data, columns=['Day', 'TickTime', 'Speed'])
 
-connection = dict(database='db', user='default', password='ubuntu')
-ph.to_clickhouse(df, 'table', connection=connection, index=False) #Переносим данные в ClickHouse
+print('Медиана = ', df.groupby(['Day'])['Speed'].median())
+print('Минимальное значение времени', df.groupby(['Day'])['TickTime'].min())
+print('Максимальное значение времени:', df.groupby(['Day'])['TickTime'].max())
+
