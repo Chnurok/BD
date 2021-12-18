56a8f271b62c6102af4a648c0b461a9e970f0aa4
Mikhail Smusev <39839108+Chnurok@users.noreply.github.com>
2021-12-18 12:29:14 +0300

    Update 8.sql

--- 8 lab/8.sql
+++ 8 lab/8.sql
@@ -3,4 +3,4 @@ Create database db
 CREATE TABLE db.table (TickTime double, Speed double, Day int) ENGINE=Memory
 
 SELECT TickTime, Day FROM db.table
-WHERE (Day=1 and Speed > 5827816) or (Day=2 and Speed > 4271012) or (Day=3 and Speed > 3885239) or (Day=7 and Speed > 1656648) or (Day=7 and Speed > 3671805)
+WHERE (Day=1 and Speed > 5827816) or (Day=2 and Speed > 4271012) or (Day=3 and Speed > 3885239) or (Day=4 and Speed > 1656648) or (Day=7 and Speed > 3671805)
